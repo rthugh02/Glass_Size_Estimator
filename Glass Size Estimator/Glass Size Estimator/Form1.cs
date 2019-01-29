c51a81140ce42589a37f9120f76b64dced7c501c
Read Hughes <hughes.read@gmail.com>
2019-01-29 16:21:24 -0500

    Showing example of dynamic controller loading in winforms
    This shows how layoutpanels can be used to add dynamic controller elements to a winforms application. The way we have discussed the design of th is project, we will need to parse a configuration file with the available options, and use the settings to build the form on the fly.

--- Glass Size Estimator/Glass Size Estimator/Form1.cs
+++ Glass Size Estimator/Glass Size Estimator/Form1.cs
@@ -15,6 +15,16 @@ namespace Glass_Size_Estimator
         public Form1()
         {
             InitializeComponent();
+
+            TableLayoutPanel layoutPanel = new TableLayoutPanel();
+            layoutPanel.Size = new Size(620, 100);
+            layoutPanel.Location = new Point(10, 20);
+
+            layoutPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+            layoutPanel.Controls.Add(new Label { Text = "Testing" });
+
+            this.Controls.Add(layoutPanel);
         }
     }
 }
