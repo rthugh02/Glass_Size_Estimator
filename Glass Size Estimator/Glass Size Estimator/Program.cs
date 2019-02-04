2238669ea1c285a150387d1bb41ef939dba972c2
Read Hughes <hughes.read@gmail.com>
2019-02-04 12:56:30 -0500

    Changed JSON importing to use dynamic object to test state values for building correct state classes
    State is an abstract class and can't be instantiated, therefore we treat the JSON as a dynamic and test the Operation property of each state to see what class it corresponds with. The example I added shows how we might build a DivisionState given what was read from the JSON object.

--- Glass Size Estimator/Glass Size Estimator/Program.cs
+++ Glass Size Estimator/Glass Size Estimator/Program.cs
@@ -30,8 +30,24 @@ namespace Glass_Size_Estimator
                 using (var stream = new StreamReader(file))//put a test file in your bin/Debug/ folder to see this in action
                 {
                     string json_data = stream.ReadToEnd();
-                    ProductLine productLine = JsonConvert.DeserializeObject<ProductLine>(json_data);
-                    configsRead.Add(productLine);
+                    dynamic productLine = JsonConvert.DeserializeObject<Object>(json_data); //A dynamic object has dynamic runtime properties that can be referenced even though the compiler doesn't know what they are
+                    foreach (var state in productLine.Logic)
+                    {
+                        if ((new List<string> { "Addition", "Subtraction", "Multiplication", "Division" }).Any(s => 
+                        s.Equals((string)state.Operation, StringComparison.OrdinalIgnoreCase)))
+                        {
+                            //As an example, if we read in this JSON object and loop through each of the states, we can test if they are an ArithmeticState with this conditional
+                            //For simplicity, assuming this is a division state:
+                            DivisionState dState = new DivisionState();
+                            dState.Input = state.Input.Value;
+                            dState.NextState = (int)state.NextStep.Value;
+                            dState.Operation = state.Operation.Value;
+                            dState.StateNumber = (int)state.StepNumber.Value;
+                            dState.Value = (int)state.Value.Value;
+                            
+                        }
+                    }
+                    //configsRead.Add(productLine);
                 }
             }
 
