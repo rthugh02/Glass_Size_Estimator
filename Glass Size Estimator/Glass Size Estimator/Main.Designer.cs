3fcd3bab2afc2d951687f9b29e95c8e55eeebd65
Read Hughes <hughes.read@gmail.com>
2019-02-05 14:41:15 -0500

    Anchored elements to form
    Anchored static elements to form so that it can be adjusted as user desires

--- Glass Size Estimator/Glass Size Estimator/Main.Designer.cs
+++ Glass Size Estimator/Glass Size Estimator/Main.Designer.cs
@@ -28,24 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.RestButton = new System.Windows.Forms.Button();
             this.ExportButton = new System.Windows.Forms.Button();
             this.MiscButton = new System.Windows.Forms.Button();
             this.ProductLineSelector = new System.Windows.Forms.ListView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // RestButton
-            // 
-            this.RestButton.Location = new System.Drawing.Point(333, 633);
-            this.RestButton.Name = "RestButton";
-            this.RestButton.Size = new System.Drawing.Size(178, 43);
-            this.RestButton.TabIndex = 1;
-            this.RestButton.Text = "Reset";
-            this.RestButton.UseVisualStyleBackColor = true;
             // 
             // ExportButton
             // 
-            this.ExportButton.Location = new System.Drawing.Point(667, 633);
+            this.ExportButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.ExportButton.Location = new System.Drawing.Point(287, 3);
             this.ExportButton.Name = "ExportButton";
             this.ExportButton.Size = new System.Drawing.Size(178, 43);
             this.ExportButton.TabIndex = 2;
@@ -54,7 +48,8 @@
             // 
             // MiscButton
             // 
-            this.MiscButton.Location = new System.Drawing.Point(1005, 633);
+            this.MiscButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.MiscButton.Location = new System.Drawing.Point(539, 3);
             this.MiscButton.Name = "MiscButton";
             this.MiscButton.Size = new System.Drawing.Size(178, 43);
             this.MiscButton.TabIndex = 3;
@@ -63,34 +58,68 @@
             // 
             // ProductLineSelector
             // 
+            this.ProductLineSelector.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ProductLineSelector.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ProductLineSelector.Location = new System.Drawing.Point(12, 12);
             this.ProductLineSelector.Name = "ProductLineSelector";
-            this.ProductLineSelector.Size = new System.Drawing.Size(218, 664);
+            this.ProductLineSelector.Size = new System.Drawing.Size(307, 664);
             this.ProductLineSelector.TabIndex = 4;
             this.ProductLineSelector.UseCompatibleStateImageBehavior = false;
             this.ProductLineSelector.View = System.Windows.Forms.View.List;
             this.ProductLineSelector.SelectedIndexChanged += new System.EventHandler(this.ProductLineSelector_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.MiscButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ResetButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ExportButton, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(368, 625);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(754, 49);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.ResetButton.Location = new System.Drawing.Point(36, 3);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(178, 43);
+            this.ResetButton.TabIndex = 4;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1322, 696);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.ProductLineSelector);
-            this.Controls.Add(this.MiscButton);
-            this.Controls.Add(this.ExportButton);
-            this.Controls.Add(this.RestButton);
             this.Name = "Main";
             this.Text = "Glass Estimator";
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button RestButton;
         private System.Windows.Forms.Button ExportButton;
         private System.Windows.Forms.Button MiscButton;
         private System.Windows.Forms.ListView ProductLineSelector;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button ResetButton;
     }
 }
 
